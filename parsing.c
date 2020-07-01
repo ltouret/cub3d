cac8b59750f85009e6a0e0ee4945a6b912b2a662
LT <ltouretv@gmail.com>
2020-07-01 21:15:22 +0200

    some norm work, cut parsing func in two. updated map

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/05 22:30:27 by ltouret           #+#    #+#             */
-/*   Updated: 2020/07/01 20:19:05 by ltouret          ###   ########.fr       */
+/*   Updated: 2020/07/01 20:52:01 by ltouret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void	init_t_map(t_ok_map *map)
 	map->player = 0;
 	map->map_sta = 0;
 	map->map_end = 0;
-	//ft_printf("init done\n");
 }
 
 void	init_data(t_data *data)
@@ -45,7 +44,6 @@ void	init_data(t_data *data)
 
 int		missing_data(t_ok_map *map)
 {
-
 	if (map->r == 0)
 		return (ERR_MISS_R);
 	if (map->no == 0)
@@ -71,7 +69,7 @@ int		missing_data(t_ok_map *map)
 
 int		num_len(int num)
 {
-	int i;
+	int		i;
 
 	i = 0;
 	while (num != 0)
@@ -84,10 +82,10 @@ int		num_len(int num)
 
 int		check_res(char *line)
 {
-	int i;
+	int		i;
 
 	i = 1;
-	while(line[i])
+	while (line[i])
 	{
 		if (line[i] != ' ' && ft_isdigit(line[i]) == 0)
 			return (ERR_RES);
@@ -96,13 +94,12 @@ int		check_res(char *line)
 	return (OK);
 }
 
-int		get_reso(int *map_res, char *line, t_data *data) // re code split!
+int		get_reso(int *map_res, char *line, t_data *data)
 {
 	int		i;
 
-	//add check_res func here! if works keep goind else stop return bad reso
 	if (!check_res(line) == ERR_RES)
-		return (ERR_RES); // return Error\n invalid resolution
+		return (ERR_RES);
 	i = 1;
 	while (line[i] == ' ')
 		i++;
@@ -112,10 +109,8 @@ int		get_reso(int *map_res, char *line, t_data *data) // re code split!
 		i++;
 	data->height = ft_atoi(line + i);
 	i += num_len(data->height);
-	//ft_printf("wid %d - hei %d\n", data->width, data->height);
-	if (!(line[i] == '\0' && data->height > 0 && data->width > 0))// add return with ERR res incorrect MAYBE MOT NEEDED THE \0
+	if (!(line[i] == '\0' && data->height > 0 && data->width > 0))
 		return (ERR_RES);
-	//ft_printf("good res\n");
 	*map_res = 1;
 	return (OK);
 }
@@ -137,16 +132,16 @@ int		get_text(int *map_text, char *line, char **data_text)
 	while (line[i] == ' ')
 		i++;
 	if (check_text(line + i) == ERR_TEXT)
-		return (ERR_TEXT); 
+		return (ERR_TEXT);
 	if (!(*data_text = ft_strdup(line + i)))
 		return (ERR_MAL);
 	*map_text = 1;
 	return (OK);
 }
 
-int	free_tab(char **tab, int index)
+int		free_tab(char **tab, int index)
 {
-	int i;
+	int		i;
 
 	if (tab == NULL)
 		return (OK);
@@ -171,9 +166,9 @@ int	free_tab(char **tab, int index)
 	return (OK);
 }
 
-int		check_color(char *tmp) //try this pro more
+int		check_color(char *tmp)
 {
-	int i;
+	int		i;
 
 	i = 0;
 	while (tmp[i])
@@ -204,12 +199,7 @@ int		write_color(char **str, int num)
 	return (OK);
 }
 
-void	ft_free(const char *str, ...) // try and do a multiple param free?
-{
-}
-
 int		cast_color(char **tab, char **data_color, int *map_bool)
-//TODO add usage of ERR_INV_COLOR if > 255
 {
 	int		i;
 	char	*tmp;
@@ -221,7 +211,8 @@ int		cast_color(char **tab, char **data_color, int *map_bool)
 			return (ERR_COLOR);
 		if (ft_atoi(tab[i]) > 255 && free_tab(tab, 3))
 			return (ERR_INV_COLOR);
-		if (write_color(&tab[i], ft_atoi(tab[i])) == ERR_MAL && free_tab(tab, 3))
+		if (write_color(&tab[i], ft_atoi(tab[i])) == ERR_MAL &&
+			free_tab(tab, 3))
 			return (ERR_MAL);
 	}
 	tmp = ft_strjoin(tab[0], tab[1]);
@@ -271,21 +262,10 @@ int		get_color(int *map_bool, char *line, char **data_color)
 	return (cast_color(tab, data_color, map_bool));
 }
 
-int		map_last(t_ok_map *map)
-// checks if map is the last element of the file
-// this shit is worng!
-{
-	if (map->r && map->no && map->so && map->we && map->ea && map->s && map->f
-		&& map->c)
-		return (OK);
-	ft_printf("map not last!\n");
-	return (ERR); // return error of ERR_MAP_NOT_LAST
-}
-
 int		check_map(char *line, t_ok_map *map)
 {
 	int		i;
-	
+
 	i = 0;
 	while (line[i])
 	{
@@ -305,7 +285,6 @@ int		check_map(char *line, t_ok_map *map)
 }
 
 int		map_start(char *line)
-//TODO check the return code of this. seems fishy
 {
 	int		i;
 	int		ret_code;
@@ -330,7 +309,7 @@ int		get_map(t_ok_map *map, char *line, t_data *data)
 	{
 		if (!(data->map = malloc(sizeof(char *) * 1)))
 			return (ERR_MAL);
-		data->map[0] = NULL; 
+		data->map[0] = NULL;
 	}
 	i = 0;
 	while (data->map[i])
@@ -354,32 +333,53 @@ int		check_dup(t_ok_map *map, char *line)
 
 	ret_code = OK;
 	if (!ft_strncmp(line, "R ", 2) && map->r == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	else if (!ft_strncmp(line, "NO ", 3) && map->no == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	else if (!ft_strncmp(line, "SO ", 3) && map->so == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	else if (!ft_strncmp(line, "WE ", 3) && map->we == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	else if (!ft_strncmp(line, "EA ", 3) && map->ea == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	else if (!ft_strncmp(line, "S ", 2) && map->s == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	else if (!ft_strncmp(line, "F ", 2) && map->f == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	else if (!ft_strncmp(line, "C ", 2) && map->c == 1)
-		ret_code = ERR_DUP_KEY; 
+		ret_code = ERR_DUP_KEY;
 	return (ret_code);
 }
 
-int		parsing(t_ok_map *map, char *line, t_data *data) //change ret vals to actual func
-// TODO fix, this shit is brojken if no space in between key and value
-// TODO how to handle case that map starts with 0? send error now or later with check mvalid map?
+int		parsing2(t_ok_map *map, char *line, t_data *data)
 {
-	int ret_code; // this func will get the ret of the other func and handle if error
+	int		ret_code;
 
 	ret_code = 0;
-	if (check_dup(map, line) == ERR_DUP_KEY) // maybe get this in read_file func?
+	if (!ft_strncmp(line, "C ", 2) && map->c == 0 && map->map_sta == 0)
+		ret_code = get_color(&map->c, line, &data->c_color);
+	else if (map_start(line) == OK && map->map_end != 1)
+		ret_code = get_map(map, line, data);
+	else if (map->map_end == 1 && line[0] != '\0')
+		ret_code = ERR_MAP_NOT_LAST;
+	else if (line[0] == '\0')
+	{
+		ret_code = OK;
+		if (map->map_sta == 1)
+			map->map_end = 1;
+	}
+	else
+		ret_code = ERR_INV_KEY;
+	return (ret_code);
+}
+
+// TODO how to handle case that map starts with 0? send error now or later with check mvalid map?
+int		parsing(t_ok_map *map, char *line, t_data *data)
+{
+	int		ret_code;
+
+	ret_code = 0;
+	if (check_dup(map, line) == ERR_DUP_KEY)
 	{
 		ft_printf("ret of line: %d\n", ERR_DUP_KEY);
 		return (ERR_DUP_KEY);
@@ -398,41 +398,21 @@ int		parsing(t_ok_map *map, char *line, t_data *data) //change ret vals to actua
 		ret_code = get_text(&map->s, line, &data->s_text);
 	else if (!ft_strncmp(line, "F ", 2) && map->f == 0 && map->map_sta == 0)
 		ret_code = get_color(&map->f, line, &data->f_color);
-	else if (!ft_strncmp(line, "C ", 2) && map->c == 0 && map->map_sta == 0)
-		ret_code = get_color(&map->c, line, &data->c_color);
-	else if (map_start(line) == OK && map->map_end != 1) //in case of map add if only at the end of file!!!!!
-		ret_code = get_map(map, line, data); // call map func here
-	else if (map->map_end == 1 && line[0] != '\0') //map not last
-		ret_code = ERR_MAP_NOT_LAST;
-	else if (line[0] == '\0') // in case of empty line in the file
-	{
-		ret_code = OK;
-		if (map->map_sta == 1)
-			map->map_end = 1;
-	}
 	else
-		ret_code = ERR_INV_KEY;
-	//ft_printf("ret of line: %d\n", ret_code);
+		ret_code = parsing2(map, line, data);
 	return (ret_code);
 }
 
-int		read_file(int fd, t_ok_map *map, t_data *data) // uncomment to use ft_split
+int		read_file(int fd, t_ok_map *map, t_data *data)
 {
 	int		gnl;
 	int		ret_code;
 	char	*line;
-	//char **arr;
 
-	//int i = 0;
-
-	line = NULL;
 	gnl = 1;
-	//while ((gnl = get_next_line(fd, &line)) > 0)
 	while (gnl == 1)
 	{
 		gnl = get_next_line(fd, &line);
-		//arr = ft_split(line + 1, ' ');
-		//line = ft_strtrim(line, "\f\t \n\r\v"); //trim or not trim spaces?
 		ft_printf("%s\n", line);
 		if ((ret_code = parsing(map, line, data)) != OK)
 		{
@@ -441,14 +421,6 @@ int		read_file(int fd, t_ok_map *map, t_data *data) // uncomment to use ft_split
 			return (ret_code);
 		}
 		free(line);
-		/*while (arr[i] != NULL)
-		{
-			ft_printf("%s\n", arr[i]);
-			free(arr[i]);
-			i++;
-		}
-		free(arr);
-		gnl = 0;*/
 	}
 	if ((ret_code = missing_data(map)) != OK)
 	{
@@ -495,22 +467,20 @@ int		open_fd(char *filename)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (ERR_NO_FILE); // return 
+		return (ERR_NO_FILE);
 	return (fd);
 }
 
 int		handle_args(int argc, char **argv)
-//TODO keep argv[1] (filename) in memory in struct
-// handle that the argv[2] must be "--save" with strcmp.
 {
 	if (argc < 2)
 	{
 		ft_printf("not enough args\n");
-		return (ERR_FEW_ARG); // change TOO FEW ARGS
+		return (ERR_FEW_ARG);
 	}
 	else if (argc > 3)
 	{
-		ft_printf("too many args\n"); // change TOO MANY ARGS
+		ft_printf("too many args\n");
 		return (ERR_MANY_ARG);
 	}
 	if (argc == 3 && ft_strcmp("--save", argv[2]) != 0)
@@ -521,13 +491,13 @@ int		handle_args(int argc, char **argv)
 	return (OK);
 }
 
+//TODO remove printfs, and create func that prints error msgs!
 int		main(int argc, char **argv)
 {
 	t_ok_map	*map;
 	t_data		*data;
 	int			fd;
 	int			ret_code;
-
 
 	if ((ret_code = handle_args(argc, argv)) != OK)
 		return (ret_code);
@@ -537,7 +507,7 @@ int		main(int argc, char **argv)
 	if ((ret_code = fd) == ERR_NO_FILE)
 	{
 		ft_printf("no file\n");
-		return (ret_code); // return ERR_NO_FILE
+		return (ret_code);
 	}
 
 	if (!(map = malloc(sizeof(t_ok_map))))
